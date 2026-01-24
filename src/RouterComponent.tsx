1e57417c30621aad56cad26dcfdb0a4c657664ee
eugenekkkhan <eugenekhananov@yandex.ru>
2026-01-24 17:11:33 +0300

    feat: add RouterComponent with basic routing setup

---
+++ src/RouterComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes, BrowserRouter } from 'react-router'
+
+const RouterComponent = () => {
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>hhh</div>}/>
+        <Route path='/faq' element={<div>iou</div>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouterComponent
