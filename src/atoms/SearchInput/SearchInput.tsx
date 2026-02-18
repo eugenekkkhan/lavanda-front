c01a1f5a7f247f41924265a67e982e35d6c41051
Anton Klintsov <klincovanton123@gmail.com>
2026-02-18 18:38:33 +0300

    style: add style to SearchInput

--- src/atoms/SearchInput/SearchInput.tsx
+++ src/atoms/SearchInput/SearchInput.tsx
@@ -1,18 +1,28 @@
 import { InputHTMLAttributes } from 'react'
-import { CiSearch } from "react-icons/ci";
-import { RxCross1 } from "react-icons/rx";
+import { CiSearch } from 'react-icons/ci'
+import { RxCross1 } from 'react-icons/rx'
 
-
-interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement>{
-	placeholder?:string
+interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
+	placeholder?: string
 }
 
-const SearchInput = ({ placeholder='Поиск', className = ''}:SearchInputProps) => {
+const SearchInput = ({
+	placeholder = 'Поиск',
+	className = '',
+}: SearchInputProps) => {
 	return (
-		<div className="inline-flex items-center justify-center gap-2">
-		<CiSearch />
-		<input className={`${className}`} type="text" placeholder={placeholder} />
-		<RxCross1 />
+		<div className='border-b-[1px] border-white w-fit mx-3'>
+
+			<div className='flex items-center justify-between gap-1 p-3'>
+		
+				<CiSearch color='white' size={24} />
+				<input
+					className={`${className} bg-transparent text-white text-base focus:outline-none flex-1 mx-2`}
+					type='text'
+					placeholder={placeholder}
+				/>
+				<RxCross1 color='white' size={24} className='cursor-pointer' />
+			</div>
 		</div>
 	)
 }
