65146f6171a06b2ee40156ef0ae1e9daa2063b88
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-02-28 19:32:15 +0300

    feat: ContactsPill added

---
+++ src/molecules/Contacts/ContactPill.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import IconButton from "../../molecules/Buttons/IconButton";
+import { contactsData } from "../../organisms/ContactSection/contacts.data";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import TextButton from "../../molecules/Buttons/TextButton";
+
+interface ContactPillProps {
+  displayText: boolean,
+  displayAddress: boolean,
+}
+
+const ContactPill: React.FC<ContactPillProps> = ({
+  displayText = false,
+  displayAddress = false
+}) => {
+  return (
+    <motion.div 
+      className="bg-purple-400 rounded-[21px] text-white p-4 overflow-hidden"
+      initial={{ height: 0, opacity: 0 }}
+      animate={{ height: "auto", opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.3 }}
+      >
+        <div className="flex flex-wrap items-center gap-3 w-full">
+          <div className="flex gap-3 justify-start shrink-0">
+            {contactsData.map((contact) => (
+              <a key={contact.id} href={contact.href} target="_blank" rel="noopener noreferrer">
+                <IconButton icon={contact.icon} className="rounded-full !p-[8px]" theme="primary" />
+              </a>
+            ))}
+          </div>
+
+          <div className="grow min-w-[200px]">
+            <a href="mailto:info@lavandamed.ru" className="block w-full">
+              <IconButton 
+                icon={MdOutlineAlternateEmail} 
+                className="!p-[4px] w-full justify-center items-center" 
+                theme="primary"
+              >
+                <span className="truncate">info@lavandamed.ru</span>
+              </IconButton>
+            </a>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6 }}
+      >
+        <div className="flex flex-col mt-3 w-full">
+          {displayText && <span className="mb-2">Телефоны:</span>}
+          <div className="flex flex-col sm:flex-row gap-3 sm:gap-3 w-full">
+            <a href="tel:+79802444401" className="flex-1 w-full sm:w-auto">
+              <TextButton 
+                text="+ 7&nbsp;(980)&nbsp;244&#8209;44&#8209;01" 
+                className="!p-[4px] w-full justify-center" 
+                theme="primary" 
+              />
+            </a>
+            <a href="tel:+79802444400" className="flex-1 w-full sm:w-auto">
+              <TextButton 
+                text="+ 7&nbsp;(980)&nbsp;244&#8209;44&#8209;00" 
+                className="!p-[4px] w-full justify-center" 
+                theme="primary" 
+              />
+            </a>
+          </div>
+        </div>
+      </motion.div>
+
+      {displayAddress && (
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.9 }}
+        >
+          <div className="flex flex-col mt-3 w-full">
+            {displayText && <span className="mb-2">Адрес:</span>}
+            <TextButton 
+              text="г. Лиски, Воронежская область, ул Титова д. 20 помещение XI"
+              theme="primary"
+              size="lg"
+            />
+          </div>
+        </motion.div>
+      )}
+    </motion.div>
+  );
+};
+
+export default ContactPill;
--- src/organisms/ContactSection/ContactSection.tsx
+++ src/organisms/ContactSection/ContactSection.tsx
@@ -1,8 +1,5 @@
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-import IconButton from "../../molecules/Buttons/IconButton";
-import { contactsData } from "./contacts.data";
-import { MdOutlineAlternateEmail } from "react-icons/md";
-import TextButton from "../../molecules/Buttons/TextButton";
+import ContactPill from "../../molecules/Contacts/ContactPill";
 
 const ContactSection = () => {
   return (
@@ -12,60 +9,14 @@ const ContactSection = () => {
           <h2 className="text-4xl md:text-5xl font-bold text-purple-400 mb-4">
             Контакты
           </h2>
-          <div className="flex flex-col gap-[10px]">
-            <span>Мы в социальных сетях:</span>
-            <div className="flex flex-row w-[176px] justify-between">
-              {contactsData.map((contact) => (
-                <a
-                  key={contact.id}
-                  href={contact.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <IconButton
-                    icon={contact.icon}
-                    className="rounded-full !p-[6px] !text-black border border-black border-solid"
-                  />
-                </a>
-              ))}
-            </div>
+          <div className="hidden sm:block">
+            <ContactPill displayAddress={true} displayText={true}/>
           </div>
-
-          <div className="flex flex-col gap-[10px]">
-            <span>Mail:</span>
-            <a href="mailto:info@lavandamed.ru">
-              <IconButton 
-                icon={MdOutlineAlternateEmail}
-                className="flex flex-row !w-[230px] !p-[2px] !text-black border border-black border-solid"
-              >
-                info@lavandamed.ru
-              </IconButton>
-            </a>
-          </div>
-
-          <div className="flex flex-col gap-[10px]">
-            <span>Телефоны:</span>
-            <a href="tel:+79802444401">
-              <TextButton 
-                text="+ 7&nbsp;(980)&nbsp;244&#8209;44&#8209;01"
-                className="!w-[205px] !p-[2px] !text-black border border-black border-solid"
-              />
-            </a>
-            <a href="tel:+79802444400">
-              <TextButton 
-                text="+ 7&nbsp;(980)&nbsp;244&#8209;44&#8209;00"
-                className="!w-[205px] !p-[2px] !text-black border border-black border-solid"
-              />
-            </a>
-          </div>
-
-          <div className="flex flex-col gap-[10px]">
-            <span>Адрес:</span>
-            <span>г. Лиски, Воронежская область,</span>
-            <span>ул Титова д.20 помещение XI</span>
+          <div className="block sm:hidden">
+            <ContactPill displayAddress={true} displayText={false}/>
           </div>
         </div>
-        <div className="w-[641px] h-[520px] rounded-3xl overflow-hidden">
+        <div className="w-fit lg:w-[641px] h-[520px] rounded-3xl overflow-hidden">
           <YMaps>
             <Map 
               defaultState={{ center: [50.985008, 39.480560], zoom: 16.5 }}
