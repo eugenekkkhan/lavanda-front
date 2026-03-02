6a83ede17086766d02b7b98bfe43102968404442
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-03-02 14:02:05 +0300

    improved css

--- src/organisms/Footer/FooterSection.tsx
+++ src/organisms/Footer/FooterSection.tsx
@@ -4,21 +4,31 @@ import IconButton from "../../molecules/Buttons/IconButton";
 import { IoMdArrowRoundUp } from "react-icons/io";
 import { scroller } from "react-scroll"
 import ContactPill from "../../molecules/Contacts/ContactPill";
+// import logo from "../../assets/Logo.svg"
 
 const FooterSection = () => {
   return (
     <motion.section className="w-full bg-primary py-16 md:py-24 px-4 snap-start snap-always">
       <motion.div className="max-w-[1104px] min-h-[600px] mx-auto text-base text-accent relative">
-          <div className="flex flex-wrap gap-8 max-w-7xl">
+          <div className="flex flex-col md:flex-row md:flex-wrap gap-8 max-w-7xl min-h-[300px]">
             
-            <div className="md:block hidden">
-              <div className="flex flex-col min-w-[200px] flex-1">
-                <div className="font-bold text-xl">ЛАВАНДА</div>
-                <div className="text-xl text-gray-600">МЕДИЦИНСКИЙ ЦЕНТР</div>
+            <div className="lg:block hidden">
+              <div className="relative">
+                <h1 className="text-lg md:text-xl lg:text-2xl font-bold text-accent mb-4 text-nowrap">
+                  Медицинский центр
+                </h1>
+                <img 
+                  // src={logo} 
+                  className="absolute top-1/2 left-1/4
+                    scale-70 md:scale-100 lg:scale-125
+                    -translate-x-16.5 md:-translate-x-9.5 lg:-translate-x-3
+                    -translate-y-1.5 md:translate-y-2.5 lg:translate-y-6 z-10"
+                  alt="Logo"
+                />
               </div>
             </div>
 
-            <div className="flex flex-col min-w-[160px] flex-1">
+            <div className="flex flex-col min-w-[170px] flex-1">
               <div className="font-semibold text-xl text-secondary mb-2">ИНФОРМАЦИЯ</div>
               <div className="flex flex-col text-sm gap-2">
                 <div>Врачи</div>
@@ -27,7 +37,7 @@ const FooterSection = () => {
               </div>
             </div>
 
-            <div className="flex flex-col min-w-[200px] flex-1">
+            <div className="flex flex-col min-w-[200px] flex-2">
               <div className="font-semibold text-xl text-secondary mb-2">ЧАСЫ РАБОТЫ</div>
               
               <div className="flex flex-col h-full">
@@ -64,13 +74,13 @@ const FooterSection = () => {
               </div>
             </div>
 
-            <div className="flex flex-col min-w-[220px] flex-1">
+            <div className="flex flex-col min-w-[220px] flex-2">
               <div className="font-semibold text-xl text-secondary mb-2">КОНТАКТЫ</div>
               <div className="flex flex-col gap-2 text-sm">
                 <div>г. Лиски, Воронежская область, ул. Титова, д. 20, помещение XI</div>
                 <div>Остановка «ТРЦ Проспект»</div>
-                <div className="mt-2">+ 7 (980) 244-44-00</div>
-                <div>info@lavandamed.ru</div>
+                <div className="md:block hidden mt-2">+ 7 (980) 244-44-00</div>
+                <div className="md:block hidden">info@lavandamed.ru</div>
               </div>
             </div>
           </div>
@@ -84,38 +94,40 @@ const FooterSection = () => {
                   <p>Вы можете написать нам любым удобным для вас способом - телефон, почта, мессенджеры или социальные сети.</p>
                 </div>
               </div>
-              <div className="mx-auto lg:mx-0">
+              <div className="mx-auto lg:mx-0 w-full">
                 <ContactPill displayText={false} displayAddress={false} />
               </div>
             </div>
-            
+
             <div className="flex flex-row justify-between text-nowrap item-center">
-              <span className="text-2xl text-secondary">© lavandamed.ru, 2026</span>
-               <a href="https://lavandamed.ru/privacy.php" className="sm:w-auto">
-                  <TextButton
-                    text="Политика конфиденциальности"
-                    size="base"
-                    className="!py-[10px] w-full justify-center"
-                  />
-                </a>
+              <span className="text-xl md:text-2xl text-secondary">© lavandamed.ru, 2026</span>
+
+              {/* <a href="https://lavandamed.ru/privacy.php" className="sm:w-auto">
+                <TextButton
+                  text="Политика конфиденциальности"
+                  size="base"
+                  className="!py-[10px] w-full justify-center"
+                />
+              </a> */}
+              <div className="mr-[9px]">
+              <IconButton
+                icon={IoMdArrowRoundUp}
+                className="text-accent !rounded-full !p-[8px]"
+                side="only"
+                onClick={() => {
+                  scroller.scrollTo("home", {
+                      duration: 2000,
+                      delay: 0,
+                      smooth: true,
+                      offset: 0,
+                    });
+                }}
+              />
+              </div>
             </div>
           </div>
 
-          <div className="absolute right-1/12 bottom-1/12 translate-x-40 -translate-y-1/2">
-            <IconButton
-              icon={IoMdArrowRoundUp}
-              className="text-accent !rounded-full !p-[8px]"
-              side="only"
-              onClick={() => {
-                 scroller.scrollTo("home", {
-                    duration: 2000,
-                    delay: 0,
-                    smooth: true,
-                    offset: 0,
-                  });
-              }}
-            />
-          </div>
+
       </motion.div>
     </motion.section>
   )
