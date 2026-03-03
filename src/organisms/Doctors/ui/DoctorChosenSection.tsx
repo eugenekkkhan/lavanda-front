b180f3dcef49ea74f900a654b2b26501bec4154e
eugenekkkhan <eugenekhananov@yandex.ru>
2026-03-03 14:25:13 +0300

    fix(DoctorChosenSection): adjust padding for consistent layout

--- src/organisms/Doctors/ui/DoctorChosenSection.tsx
+++ src/organisms/Doctors/ui/DoctorChosenSection.tsx
@@ -82,7 +82,7 @@ const DoctorChosenSection = () => {
   };
 
   return (
-    <motion.section className="w-full bg-primary pt-[78px] pb-[16px] px-4 snap-start snap-always">
+    <motion.section className="w-full bg-primary py-[16px] px-4 snap-start snap-always">
       <motion.div className="max-w-[1104px] mx-auto min-h-[800px]">
         <div className="w-full flex items-center mt-[58px] mb-4">
           <motion.div className="">
