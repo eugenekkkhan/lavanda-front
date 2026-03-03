9c05583cda2dd0e9386bf3301566aa077dde739f
shamshinmax <mksmsmsn@gmail.com>
2026-03-03 06:28:03 +0000

    feat(ALLOWEWD_HOSTS)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) =>{
+  const env = loadEnv(mode, process.cwd(), ''); // Use '' as the prefix to load all variables, not just VITE_*
+  // Split the string from the .env file into an array of strings, or set to true
+  const allowedHosts = env.ALLOWED_HOSTS === 'true' ? true : env.ALLOWED_HOSTS.split(',');
+  return {
+  server: {
+    allowedHosts: allowedHosts,
+    host: '0.0.0.0', // or true
+    // You can also specify the port if needed, e.g., port: 3000,
+  },
   plugins: [react(), tailwindcss()],
+}
 });
